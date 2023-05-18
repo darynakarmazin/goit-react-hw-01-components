28f13284f3cbe33bf10301ee18aed6258c6baefc
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-05-18 22:33:15 +0300

    Added Profile component

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li key={stat.id} className={css.item}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
